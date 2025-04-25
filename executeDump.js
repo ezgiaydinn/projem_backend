c2727dbc89b374a7e62414ab2848a224ecc58971
Ezgi Aydın <121128488+ezgiaydinn@users.noreply.github.com>
2025-04-25 16:31:15 +0300

    dumpi eklendi

---
+++ executeDump.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const mysql = require("mysql2");
+
+// Railway ortam değişkenlerini kullanıyoruz
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    multipleStatements: true,
+  });
+  
+
+// dump.sql içeriğini oku
+const dump = fs.readFileSync("dump.sql", "utf8");
+
+// dump’ı çalıştır
+connection.query(dump, (err, results) => {
+  if (err) {
+    console.error("❌ Dump çalıştırma hatası:", err);
+    process.exit(1);
+  }
+  console.log("✅ Dump başarıyla içeri aktarıldı!");
+  process.exit(0);
+});
