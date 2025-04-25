d1c7813f2170e545dd5c8b6ab3c9a582058b2a30
Ezgi Aydın <121128488+ezgiaydinn@users.noreply.github.com>
2025-04-25 16:37:23 +0300

    dumpi eklendi

--- executeDump.js
+++ executeDump.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER,
-    password: ofljmHMoSHlqfAOWyjaZPyMadWoDODLS,
+    password: "ofljmHMoSHlqfAOWyjaZPyMadWoDODLS",
     database: process.env.DB_NAME,
     multipleStatements: true,
   });
