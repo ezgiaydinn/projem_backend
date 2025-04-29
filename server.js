7d2af0199c832c2df2fc61a35c81a9430a8db6a9
Ezgi Aydın <121128488+ezgiaydinn@users.noreply.github.com>
2025-04-29 11:45:33 +0300

    favori sildim

--- server.js
+++ server.js
@@ -197,8 +197,8 @@ app.post('/api/auth/uploadProfileImage', upload.single('image'), (req, res) => {
   });
 });
 
-  const favoritesRoutes = require('./routes/favorites');
-  app.use('/api/favorites', favoritesRoutes);
+  //const favoritesRoutes = require('./routes/favorites');
+  //app.use('/api/favorites', favoritesRoutes);
 
 
 // // Kitabı kaydetme veya güncelleme
