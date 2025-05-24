d9d31d38ae7a2565048d58b78a0a47aad55f16dd
Ezgi Aydın <121128488+ezgiaydinn@users.noreply.github.com>
2025-05-24 09:41:58 +0300

    son düzenleme

--- python-recommender/recommender.py
+++ python-recommender/recommender.py
@@ -1065,6 +1065,7 @@ def recommend(
             db = mysql.connector.connect(
                 host=os.getenv("DB_HOST"),
                 user=os.getenv("DB_USER"),
+                port=int(os.getenv("DB_PORT")),
                 password=os.getenv("DB_PASSWORD"),
                 database=os.getenv("DB_NAME")
             )
